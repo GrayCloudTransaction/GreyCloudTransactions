b192d07158adbc86ade4083ed277d23793d95002
AlexandraHY14 <alexandra.yara@sptech.school>
2023-08-27 16:27:47 -0300

    monitoramento - Hard Disk e memória virtual(RAM)

--- ApiCalculo/SistemaMarise.py
+++ ApiCalculo/SistemaMarise.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime
-import math
+from math import pow
 import os
 from time import sleep
 import psutil
@@ -15,12 +15,12 @@ def MostrarMsgGCT():
     print(
     """
 
-     ██████╗ ██████╗  █████╗ ██╗   ██╗
-    ██╔════╝ ██╔══██╗██╔══██╗╚██╗ ██╔╝
-    ██║  ██╗ ██████╔╝███████║ ╚████╔╝ 
-    ██║  ╚██╗██╔══██╗██╔══██║  ╚██╔╝  
-    ╚██████╔╝██║  ██║██║  ██║   ██║   
-     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   
+     ██████╗ ██████╗ ███████╗██╗   ██╗
+    ██╔════╝ ██╔══██╗██╔════╝╚██╗ ██╔╝
+    ██║  ██╗ ██████╔╝█████╗   ╚████╔╝
+    ██║  ╚██╗██╔══██╗██╔══╝    ╚██╔╝
+    ╚██████╔╝██║  ██║███████╗   ██║
+     ╚═════╝ ╚═╝  ╚═╝╚══════╝   ╚═╝
 
      █████╗ ██╗      █████╗ ██╗   ██╗██████╗
     ██╔══██╗██║     ██╔══██╗██║   ██║██╔══██╗
@@ -61,7 +61,7 @@ def MostrarValoresCPU():
         frequenciaCpu = psutil.cpu_freq()
 
         print('-' * 100)
-        print("\n" + "Quantidade de Cores:" + str(qtdCores) + " Quantidade Threads: " + str(qtdThreads) + "\n")
+        print("\n" + "Quantidade de Cores:" + str(qtdCores) + "\nQuantidade Threads: " + str(qtdThreads) + "\n")
         print('-' * 100)
 
         print((" " * 45) + "Tempos da CPU: \n")
@@ -105,22 +105,103 @@ def MostrarValoresCPU():
 def MostrarValoresDiscoLocal():
 
     print('a')
-    # byteToMegabyte = (float(psutil.disk_usage('/').total) / 1024)
-    # megaByteToGigabyte = byteToMegabyte / 1024
-    # gigaByteToTerabyte = megaByteToGigabyte / 1024
-    # clearConsole()
-    # MostrarMsgGCT()
-    # print(psutil.disk_partitions())
-    # print(megaByteToGigabyte)
+     
+    totalDisco = psutil.disk_usage('C:\\').total
+    sendoUsado = psutil.disk_usage('C:\\').used
+    espacoLivre = psutil.disk_usage('C:\\').free
+    porcentagemEmUso = psutil.disk_usage('C:\\').percent
 
+    # byteToMegabyteTotal = (float(totalDisco) / 1024)
+    # byteToMegabyteUsando = (float(sendoUsado) / 1024)
+    # byteToMegabyteLivre = (float(espacoLivre) / 1024)
+
+    byteToGigabyteTotal = (float(totalDisco) * (1 * pow(10,-9)))
+    byteToGigabyteUsando = (float(sendoUsado) * (1 * pow(10,-9)))
+    byteToGigabyteLivre = (float(espacoLivre) * (1 * pow(10,-9)))
+
+    # megaByteToGigabyteTotal = byteToMegabyteTotal / 1024
+    # megaByteToGigabyteUsando = byteToMegabyteUsando / 1024
+    # megaByteToGigabyteLivre = byteToMegabyteLivre / 1024
+
+    # gigaByteToTerabyte = megaByteToGigabyte / 1024
+
+    while True:
+        clearConsole()
+
+        MostrarMsgGCT()
+    
+        formatted_total1 = "{:.2f}".format(byteToGigabyteTotal)
+        formatted_total2 = "{:.2f}".format(byteToGigabyteUsando)
+        formatted_total3 = "{:.2f}".format(byteToGigabyteLivre)
+
+    # print("\nQuantidade total(GB): " + str(megaByteToGigabyteTotal))
+    # print("\nQuantidade em uso(GB): " + str(megaByteToGigabyteUsando))
+    # print("\nEm uso: " + str(porcentagemEmUso) + "%" + " (com base no total e na quantidade em uso.)")
+    # print("\nQuantidade livre(GB): " + str(megaByteToGigabyteLivre) + "\n")
+
+        print("\nDe bytes para Gigabytes: ")
+        print("\nQuantidade total: " + Fore.BLUE + str(formatted_total1) + " GB" + Style.RESET_ALL + "\n")
+        print("Quantidade livre: " + Fore.BLUE + str(formatted_total3) + " GB" + Style.RESET_ALL + "\n")
+        print("Quantidade em uso: " + Fore.BLUE + str(formatted_total2) + " GB" + Style.RESET_ALL + "\n")
+    # print("Em uso: " + str(porcentagemEmUso) + "%" + " (com base no total e na quantidade em uso.)")
+
+        if(porcentagemEmUso > 70):
+            print("\n" + "Em uso: " + Fore.RED + str(porcentagemEmUso) + "%" + Style.RESET_ALL + "\n")
+        elif porcentagemEmUso > 50 :
+            print("\n" + "Em uso: " + Fore.YELLOW + str(porcentagemEmUso) + "%" + Style.RESET_ALL + "\n")
+        else :
+            print("\n" + "Em uso: " + Fore.GREEN + str(porcentagemEmUso) + "%" + Style.RESET_ALL + "\n")
+        
+        print('-' * 100)
+
+        print("Informações sem tratamento:\n")
+        print(psutil.disk_partitions())
+        print('-' * 100)
+        print(psutil.disk_usage('C:\\'))
+
+        print("=" * 100)
+        sleep(2)
+
+# Fim das Info Disco Local
 def MostrarValoresRAM():
 
     valoresMemoriaRam = psutil.virtual_memory()
+    ramTotal = valoresMemoriaRam.total
+    ramDisponivel = valoresMemoriaRam.available
+    ramPercentualUtilizado = valoresMemoriaRam.percent
+    ramUtilizando = valoresMemoriaRam.used
+    ramLivre = valoresMemoriaRam.free
+
+    ramByteToGigabyteTotal = (float(ramTotal) * (1 * pow(10,-9)))
+    ramByteToGigabyteUsando = (float(ramUtilizando) * (1 * pow(10,-9)))
+    ramByteToGigabyteDisponivel = (float(ramDisponivel) * (1 * pow(10,-9)))
+    ramByteToGigabyteLivre = (float(ramLivre) * (1 * pow(10,-9)))
 
     clearConsole()
-    print("Memória RAM total: " + str(valoresMemoriaRam.total) + "\n"
-          + "Memória RAM disponível: ")
-    print('b')
+    MostrarMsgGCT()
+
+    print("Memória RAM total: " + Fore.BLUE + "{:.2f}".format(ramByteToGigabyteTotal) + " GB" + Style.RESET_ALL + "\n")
+    print( "Memória RAM disponível: " + Fore.BLUE + "{:.2f}".format(ramByteToGigabyteDisponivel) + " GB" + Style.RESET_ALL + "\n")
+    print( "Memória RAM usado: " + Fore.BLUE + "{:.2f}".format(ramByteToGigabyteUsando) + " GB" + Style.RESET_ALL + "\n")
+    print( "Memória RAM livre: " + Fore.BLUE + "{:.2f}".format(ramByteToGigabyteLivre) + " GB" + Style.RESET_ALL + "\n")
+
+    # print( "Memória RAM percentual: " + Fore.BLUE + str(ramPercentualUtilizado) + "%" + Style.RESET_ALL + "\n")
+
+    print('-' * 100)
+
+    if(ramPercentualUtilizado > 70):
+            print("\n" + "Em uso: " + Fore.RED + str(ramPercentualUtilizado) + "%" + Style.RESET_ALL + "\n")
+    elif ramPercentualUtilizado > 50 :
+            print("\n" + "Em uso: " + Fore.YELLOW + str(ramPercentualUtilizado) + "%" + Style.RESET_ALL + "\n")
+    else :
+            print("\n" + "Em uso: " + Fore.GREEN + str(ramPercentualUtilizado) + "%" + Style.RESET_ALL + "\n")
+        
+        
+    print('-' * 100)
+    print(valoresMemoriaRam)
+
+    print("=" * 100)
+    sleep(2)
 
 def MostrarTodosValores():
     while True:
@@ -156,6 +237,7 @@ if visualizacaoDesejada == 1:
     while True:
         MostrarValoresCPU()
 elif visualizacaoDesejada == 2:
+    while True:
         MostrarValoresDiscoLocal()
 elif visualizacaoDesejada == 3:
     while True:
