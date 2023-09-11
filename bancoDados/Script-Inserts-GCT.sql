035273b8b8eea2f71f2b978c95017126c5689110
gustavodessunte <gustavo.bernardes@sptech.school>
2023-09-11 15:26:11 -0300

    Inserts consertados

--- bancoDados/Script-Inserts-GCT.sql
+++ bancoDados/Script-Inserts-GCT.sql
@@ -7,17 +7,21 @@ Select * from Empresa;
 
 Insert into Funcionario values
 (null,'Gabriel','gabriel@gmail.com','12345','presidente','09348571', '1', null, 1);
+
 insert into Funcionario values
-(null,'Raphael','raphael@gmail.com','12345','Analísta Junior',09343343, '2', 13, 1),
+(null,'Raphael','raphael@gmail.com','12345','Analísta Junior',09343343, '2', 1, 1),
 (null,'Carlos','carlos@gmail.com','12345','Analísta Sênior',09309848, '1', null, 2)
 ;
 desc Funcionario;
+
+-- Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails (`scriptgct`.`funcionario`, CONSTRAINT `funcionario_ibfk_1` FOREIGN KEY (`fkGerente`) REFERENCES `funcionario` (`idFuncionario`))
+
 
 select * from Funcionario;
 
 select * from Funcionario where email = 'gabriel@gmail.com' and senha = '12345';
 
 Insert into Maquina values
-(null, 'Servidor-18-08', 024, 10),
-(null, 'Servidor-19-07', 357, 11),
-(null, 'Servidor-14-12', 504, 12);
+(null, 'Servidor-18-08', 024, 1),
+(null, 'Servidor-19-07', 357, 2),
+(null, 'Servidor-14-12', 504, 3);
