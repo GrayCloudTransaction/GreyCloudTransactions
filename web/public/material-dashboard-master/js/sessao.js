028bf3d62e3f59be475119de5e23aacf1197017f
rafaelSchimas <rafael.ranaudo@sptech.school>
2023-10-03 15:07:18 -0300

    Arrumando dashboard

--- web/public/material-dashboard-master/js/sessao.js
+++ web/public/material-dashboard-master/js/sessao.js
@@ -2,17 +2,23 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-
+    var permissao = sessionStorage.PERMISSAO;
     if (email == undefined && nome == undefined) {
         // Swal.fire({
         //     icon: 'error',
         //     title: 'Oops...',
         //     text: `Faça login para acessar essa área do site`
         //   })
-        console.log("Faça login")
-        alert("Faça login")
+        console.log("Faça login");
+        alert("Faça login");
         window.location = "../../login.html";
-    } 
+    } else {
+        if (permissao == 1) {
+            link_dashboard.href = "../pages/dashboardGerente.html";
+        } else {
+            link_dashboard.href = "../pages/dashboardAnalista.html";
+        }
+    }
 }
 
 function limparSessao() {
