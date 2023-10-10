0775839400fe7942c426deef3682d58d7d6c7420
GabrielBifon05 <gabriel.bsouza@sptech.school>
2023-10-10 13:19:04 -0300

    fix: novo script BD e usuarioModel

--- web/src/database/SCRIPT_GREYCLOUDTRANSACTIONS_V1.0.sql
+++ web/src/database/SCRIPT_GREYCLOUDTRANSACTIONS_V1.0.sql
@@ -1,121 +1,172 @@
-DROP SCHEMA IF EXISTS `ScriptGCT` ;
-CREATE SCHEMA IF NOT EXISTS `ScriptGCT`;
-USE `ScriptGCT` ;
+-- Active: 1696953654739@@127.0.0.1@3306@ScriptGCT
+DROP DATABASE ScriptGCT;
+CREATE DATABASE IF NOT EXISTS `ScriptGCT` DEFAULT CHARACTER SET utf8 ;
+USE `ScriptGCT`;
 
-DROP TABLE IF EXISTS `Empresa` ;
+CREATE TABLE IF NOT EXISTS `componente` (
+  `id_componente` INT NOT NULL auto_increment,
+  `tipo_componente` VARCHAR(60) NULL,
+  PRIMARY KEY (`id_componente`))
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `Empresa` (
-  `idEmpresa` INT auto_increment,
-  `RazaoSocial` VARCHAR(45),
-  `CNPJ` INT,
-  `Logradouro` VARCHAR(120),
-  `Número` INT,
-  `CEP` INT,
-  `Email` VARCHAR(45),
-  `Telefone` INT,
-  PRIMARY KEY (`idEmpresa`)
-  ) auto_increment = 10;
-  
 
-  
-DROP TABLE IF EXISTS `Funcionario` ;
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `id_empresa` INT NOT NULL auto_increment,
+  `razao_social` VARCHAR(120) NULL,
+  `cnpj` CHAR(18) NULL,
+  `numero_imovel` INT NULL,
+  `cep` CHAR(9) NULL,
+  `email` VARCHAR(150) NULL,
+  `telefone` VARCHAR(13) NULL,
+  PRIMARY KEY (`id_empresa`)
+);
 
-CREATE TABLE IF NOT EXISTS `Funcionario` (
-  `idFuncionario` INT auto_increment,
-  `Nome` VARCHAR(45),
-  `Email` VARCHAR(45),
-  `Senha` VARCHAR(45),
-  `Cargo` VARCHAR(45),
-  `CPF` INT,
-  `Supervisor` INT,
-  `fkEmpresa` INT,
-  PRIMARY KEY (`idFuncionario`),
-  FOREIGN KEY (`Supervisor`) REFERENCES `Funcionario` (`idFuncionario`),
-  FOREIGN KEY (`fkEmpresa`) REFERENCES `Empresa` (`idEmpresa`)
-  ) auto_increment = 100;
+CREATE TABLE IF NOT EXISTS `chamados` (
+  `id_chamados` INT NOT NULL auto_increment,
+  `titulo` VARCHAR(60) NULL,
+  `descricao` VARCHAR(200) NULL,
+  `data_hora` DATETIME NULL,
+  `status` VARCHAR(30) NULL,
+  `fk_componente` INT NOT NULL,
+  `fk_empresa` INT NOT NULL,
+  PRIMARY KEY (`id_chamados`),
+  FOREIGN KEY (`fk_componente`) REFERENCES `componente` (`id_componente`) ON DELETE CASCADE,
+  FOREIGN KEY (`fk_empresa`) REFERENCES `empresa` (id_empresa) ON DELETE CASCADE
+);
 
-DROP TABLE IF EXISTS `Maquina` ;
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `id_funcionario` INT NOT NULL primary key auto_increment,
+  `nome` VARCHAR(120) NULL,
+  `email` VARCHAR(150) NULL,
+  `senha` VARCHAR(150) NULL,
+  `cargo` VARCHAR(90) NULL,
+  `cpf` CHAR(14) NULL,
+  `permissao` INT NOT NULL,
+  `fk_gerente` INT,
+  `fk_empresa` INT NOT NULL,
+  FOREIGN KEY (`fk_gerente`) REFERENCES funcionario(`id_funcionario`) ON DELETE CASCADE,
+  FOREIGN KEY (`fk_empresa`) REFERENCES empresa(`id_empresa`) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS `Maquina` (
-  `idMaquina` INT auto_increment,
-  `Nome` VARCHAR(45),
-  `Codigo` VARCHAR(45),
-  `fkEmpresa` INT,
-  PRIMARY KEY (`idMaquina`),
-  FOREIGN KEY (`fkEmpresa`) REFERENCES `Empresa` (`idEmpresa`)
-) auto_increment = 1000;
 
-DROP TABLE IF EXISTS `Metrica` ;
+CREATE TABLE IF NOT EXISTS `servidor` (
+  `id_servidor` INT NOT NULL auto_increment,
+  `nome` VARCHAR(60) NULL,
+  `codigo` VARCHAR(50) NULL,
+  `tipo` VARCHAR(100) NULL,
+  `descricao` VARCHAR(200) NULL,
+  `fk_empresa` INT NOT NULL,
+  PRIMARY KEY (`id_servidor`),
+  FOREIGN KEY (`fk_empresa`) REFERENCES `empresa` (`id_empresa`) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS `Metrica` (
-  `idMetrica` INT auto_increment,
-  `Metrica` VARCHAR(45),
-  PRIMARY KEY (`idMetrica`)
-  ) auto_increment = 1;
 
-DROP TABLE IF EXISTS `TipoComponente` ;
+CREATE TABLE IF NOT EXISTS `unidade_medida` (
+  `id_unidade_medida` INT NOT NULL auto_increment,
+  `unidade_medida` VARCHAR(60) NULL,
+  `tipo_medida` VARCHAR(60) NULL,
+  `fk_componente` INT NOT NULL,
+  PRIMARY KEY (`id_unidade_medida`),
+  FOREIGN KEY (`fk_componente`) REFERENCES `componente` (`id_componente`) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS `TipoComponente` (
-  `idTipoComponente` INT auto_increment,
-  `Tipo` VARCHAR(45),
-  PRIMARY KEY (`idTipoComponente`)
-  );
 
-DROP TABLE IF EXISTS `Componente` ;
+CREATE TABLE IF NOT EXISTS `modelo_componente` (
+  `id_modelo_componente` INT NOT NULL auto_increment,
+  `modelo` VARCHAR(50) NULL,
+  `fabricante` VARCHAR(60) NULL,
+  `fk_servidor` INT NOT NULL,
+  `fk_componente` INT NOT NULL,
+  `metrica_maxima` VARCHAR(45) NULL,
+  PRIMARY KEY (`id_modelo_componente`), 
+  FOREIGN KEY (`fk_servidor`) REFERENCES `servidor` (`id_servidor`) ON DELETE CASCADE,
+  FOREIGN KEY (`fk_componente`) REFERENCES `componente` (`id_componente`) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS `Componente` (
-  `idComponente` INT auto_increment,
-  `Modelo` VARCHAR(45),
-  `Marca` VARCHAR(45),
-  `fkMetrica` INT,
-  `fkTipoComponente` INT,
-  `metricaMin` INT,
-  `metricaMax` INT,
-  PRIMARY KEY (`idComponente`),
-  FOREIGN KEY (`fkMetrica`) REFERENCES `Metrica` (`idMetrica`),
-  FOREIGN KEY (`fkTipoComponente`) REFERENCES `TipoComponente` (`idTipoComponente`)
-  ) auto_increment = 10000;
+CREATE TABLE IF NOT EXISTS `registro` (
+  `id_registro` INT NOT NULL auto_increment,
+  `valor_registro` DOUBLE NULL,
+  `data_registro` DATETIME NULL,
+  `fk_modelo_componente` INT NOT NULL,
+  PRIMARY KEY (`id_registro`),
+  FOREIGN KEY (`fk_modelo_componente`) REFERENCES `modelo_componente` (`id_modelo_componente`) ON DELETE CASCADE
+);
 
-DROP TABLE IF EXISTS `Registro` ;
+-- Cadastro de Empresas
+INSERT INTO `empresa` (`razao_social`, `cnpj`, `numero_imovel`, `cep`, `email`, `telefone`) 
+VALUES ('Pague Seguro',"61.186.888/0093-01", 763, '09260-640', 'pagueSeguro@gmail.com', '123123123')
+	, ('SPTECH', "61.186.888/0093-01", 298,'09260-640', 'sptech@gmail.com', '456456456')
+	, ('Amazon', "61.186.888/0093-01", 234,'09260-640', 'amazon@gmail.com', '889877677');
+Select * from `empresa`;
 
-CREATE TABLE IF NOT EXISTS `Registro` (
-  `fkMaquina` INT auto_increment,
-  `fkComponente` INT,
-  `idRegistro` INT,
-  `ValorRegistro` INT,
-  PRIMARY KEY (`idRegistro`),
-  FOREIGN KEY (`fkMaquina`) REFERENCES `Maquina` (`idMaquina`),
-  FOREIGN KEY (`fkComponente`) REFERENCES `Componente` (`idComponente`)
-)auto_increment = 10000;
 
- -- DESC Empresa;
--- DESC Funcionario;
--- DESC Maquina;
--- DESC Metrica;
--- DESC TipoComponente;
--- DESC Componente;
--- DESC Registro;
+-- Cadastro de Gerentes
+INSERT INTO `funcionario` (`nome`, `email`, `senha`, `cargo`, `cpf`, `permissao`, `fk_gerente`, `fk_empresa`)
+VALUES ('Gabriel', 'gabriel@gmail.com', '12345', 'Gerente', '690.969.360-43', '1', null, 1)
+	, ('Fernando Brandão', 'fernado@gmail.com', '12345', 'Presidente', '226.146.010-47', '1', null, 2)
+    , ('Cláudio', 'claudio@gmail.com', '12345', 'Gerente', '810.791.190-35', '1', null, 3);
 
-INSERT INTO Empresa values 
-(null, "Coca-Cola",12345678,"Marco Aurélio", 984, 09260640, "cocacola@gmail.com", 123123123),
-(null, "SPTECH",124456789,"Haddock Lobo", 298,0609260641, "sptech@gmail.com", 456456456),
-(null, "Amazon",884456889,"Califórnia", 234,0609260641, "amazon@gmail.com", 889877677)
-;
--- Select * from Empresa;
 
-Insert into Funcionario values
-(null,"Gabriel","gabriel@gmail.com","12345","presidente",09348571, null, 10),
-(null,"Raphael","raphael@gmail.com","12345","Analísta Junior",09343343, 100, 10),
-(null,"Carlos","carlos@gmail.com","12345","Analísta Sênior",09309848, null, 11)
-;
--- select * from Funcionario;
+-- Cadastro de Funcionários
+INSERT INTO `funcionario` (`nome`, `email`, `senha`, `cargo`, `cpf`, `permissao`, `fk_gerente`, `fk_empresa`)
+values ('Cleiton Rodrigues', 'cleiton@gmail.com', '12345', 'Analísta Junior', "514.184.580-07", '2', 1, 1)
+	, ('Carlos Souza', 'carlos@gmail.com', '12345', 'Analísta Sênior', "541.886.660-56", '1', 2, 2)
+    , ('Pedro Henrique', 'pedro@gmail.com', '12345', 'Analísta Sênior', "091.045.750-67", '1', 3, 3);
 
-select * from Funcionario where email = "gabriel@gmail.com" and senha = "12345";
+SELECT * FROM `funcionario`;
 
-Insert into Maquina values
-(null, "Servidor-18-08", 024, 10),
-(null, "Servidor-19-07", 357, 11),
-(null, "Servidor-14-12", 504, 12)
-;
--- select * from Maquina;
-;
+-- Cadastro de Servidores 
+INSERT INTO `servidor` (`nome`, `codigo`, `tipo`, `descricao`, `fk_empresa`)
+VALUES ('SERVER-AHRL1NB', 'XPTO-0987', 'Servidor Principal', 'Servidor responsável por executar X tarefa', 1)
+	, ('SERVER-9HJD2AL', 'XP-9384', 'Servidor de Backup', 'Servidor responsável por backups', 1)
+    , ('SERVER-UHD71P6', 'LOC-0284', 'Servidor de Homologação', 'Servidor responsável por Homologações ', 1);
+
+
+-- Cadastro de Componentes
+INSERT INTO `componente` (`tipo_componente`) VALUES 
+('CPU'),
+('RAM'),
+('Disco');
+
+
+
+-- Cadastro de Modelo de Componentes
+INSERT INTO `modelo_componente` (`modelo`, `fabricante`, `fk_servidor`, `fk_componente`, `metrica_maxima`)
+VALUES
+('Intel Core i7 13700K', 'Intel', 1, 1, '70'),
+('AMD Ryzen 7 7800X', 'AMD', 1, 1, '70'),
+('Apple M1 Max', 'Apple', 1, 1, '70'),
+('DDR4 3200MHz 16GB', 'Corsair', 1, 2, '70'),
+('DDR5 4800MHz 32GB', 'G.Skill', 1, 2, '70'),
+('LPDDR5 6400MHz 64GB', 'Samsung', 1, 2, '70'),
+('Samsung 980 Pro 1TB', 'Samsung', 1, 3, '70'),
+('WD Black SN850 2TB', 'Western Digital', 1, 3, '70'),
+('Seagate FireCuda 530 4TB', 'Seagate', 1, 3, '70');
+
+-- Cadastro de Registros (para modelo de componentes)
+-- CPU
+INSERT INTO `registro` (`valor_registro`, `data_registro`, `fk_modelo_componente`) VALUES
+(8, "2023-10-09 14:05:32", 1), -- qtdCores
+(16, "2023-10-09 14:05:32", 1), -- qtdThreads
+(10, "2023-10-09 14:05:32", 1), -- temposCpu.user
+(20, "2023-10-09 14:05:32", 1), -- temposCpu.system
+(70, "2023-10-09 14:05:32", 1), -- temposCpu.idle
+(70, "2023-10-09 14:05:32", 1), -- UtilizacaoCore
+(70, "2023-10-09 14:05:32", 1), -- porcentagemUtilizacaoCPU
+(3.5, "2023-10-09 14:05:32", 1), -- frequenciaCpu.current
+(2.5, "2023-10-09 14:05:32", 1); -- freqCpuMin
+
+-- Disco
+INSERT INTO `registro` (`valor_registro`, `data_registro`, `fk_modelo_componente`) VALUES
+(1000, "2023-10-09 14:05:32", 7), -- Quantidade total de massa
+(750, "2023-10-09 14:05:32", 7), -- Quantidade livre de massa
+(250, "2023-10-09 14:05:32", 7), -- Quantidade de massa em uso
+(25, "2023-10-09 14:05:32", 7); -- Porcentagem em uso
+
+-- RAM
+INSERT INTO `registro` (`valor_registro`, `data_registro`, `fk_modelo_componente`) VALUES
+(16, "2023-10-09 14:05:32", 4), -- ramByteToGigabyteTotal
+(12, "2023-10-09 14:05:32", 4), -- ramByteToGigabyteDisponivel
+(4, "2023-10-09 14:05:32", 4), -- ramByteToGigabyteUsando
+(12, "2023-10-09 14:05:32", 4), -- ramByteToGigabyteLivre
+(25, "2023-10-09 14:05:32", 4); -- ramPercentualUtilizado
--- web/src/models/usuarioModel.js
+++ web/src/models/usuarioModel.js
@@ -9,7 +9,7 @@ function info(func, query){
 // Funções para exportar -- Usada por outros arquivos
 function autenticar(email, senha) {
     var instrucao = `
-        SELECT * FROM Funcionario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT * FROM funcionario WHERE email = '${email}' AND senha = '${senha}';
     `;
 
     info("Autenticar", instrucao)
